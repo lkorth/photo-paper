495744365853f80ed558d1753bdbec81987cdaff
lkorth <luke@lukekorth.com>
2015-10-16 21:31:04 -0400

    Remove wakelock from ApiService

--- PhotoPaper/src/main/java/com/lukekorth/android_500px/services/ApiService.java
+++ PhotoPaper/src/main/java/com/lukekorth/android_500px/services/ApiService.java
@@ -2,7 +2,6 @@ package com.lukekorth.android_500px.services;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.os.PowerManager;
 
 import com.lukekorth.android_500px.R;
 import com.lukekorth.android_500px.WallpaperApplication;
@@ -44,10 +43,6 @@ public class ApiService extends IntentService {
         if (Utils.shouldGetPhotos(this)) {
             mLogger.debug("Attempting to fetch new photos");
 
-            PowerManager.WakeLock wakeLock = ((PowerManager) getSystemService(POWER_SERVICE))
-                    .newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "500pxApiService");
-            wakeLock.acquire();
-
             long startTime = System.currentTimeMillis();
 
             mBus = WallpaperApplication.getBus();
@@ -59,7 +54,6 @@ public class ApiService extends IntentService {
 
             startService(new Intent(this, PhotoCacheIntentService.class));
 
-            wakeLock.release();
             mLogger.debug("Done fetching photos");
         } else {
             mBus.post(new WallpaperChangedEvent());
