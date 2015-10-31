b54d85b2270de99aa4133b7606f921e7a5035653
lkorth <luke@lukekorth.com>
2015-10-30 23:00:24 -0400

    Always send consumer key with requests

--- PhotoPaper/src/main/java/com/lukekorth/android_500px/WallpaperApplication.java
+++ PhotoPaper/src/main/java/com/lukekorth/android_500px/WallpaperApplication.java
@@ -108,10 +108,9 @@ public class WallpaperApplication extends com.activeandroid.app.Application  imp
                         BuildConfig.CONSUMER_SECRET);
                 consumer.setTokenWithSecret(accessToken.getToken(), accessToken.getTokenSecret());
                 client.interceptors().add(new SigningInterceptor(consumer));
-            } else {
-                client.interceptors().add(new ConsumerApiKeyInterceptor());
             }
 
+            client.interceptors().add(new ConsumerApiKeyInterceptor());
             client.interceptors().add(new UserAgentInterceptor());
 
             sFiveHundredPxClient = new Retrofit.Builder()
